4dd3b98f9827b4574b8bdb4b89797487a4569fe1
Zia ur Rehman <zia.rehman@withloop.co>
2022-10-12 18:39:55 +0500

    updated code

--- include/settings.php
+++ include/settings.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-define('SITE_URL', '/azeem/');
+define('SITE_URL', '/');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-// error_reporting(0);
+error_reporting(0);
 
 if(empty($_SESSION) && $loginPage != true) {
     header('location: '.SITE_URL.'login.php');
