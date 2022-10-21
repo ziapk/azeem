d100c55757e834fcb428e16188934b4fc397ee58
Zia ur Rehman <zia.rehman@withloop.co>
2022-10-21 15:07:40 +0500

    fix syntax issues and updated bulk expense

--- pages/expenses/bulk.php
+++ pages/expenses/bulk.php
@@ -30,7 +30,7 @@ echo mainHeader(['page'=> 'expense']);
                         Expense Date
                     </th>
                     <th style="vertical-align: middle; text-align: right; position: relative">
-                        <input type="text" class="form-control datepicker-single" />
+                        <input type="text" class="form-control datepicker-single" value="<?php echo date('Y-m-d');?>" />
                     </th>
                 </tr>
             </thead>
@@ -146,8 +146,10 @@ app.controller('cartController', function($scope, $http, $httpParamSerializerJQL
         $('.datepicker-single').daterangepicker({
             autoApply: true,
             maxDate: new Date(),
-            defaultValue: new Date(),
             singleDatePicker: true,
+            locale: {
+                format: 'YYYY-MM-DD'
+            },
         }, function(date) {
             $scope.form.exp_date = moment(date).format('YYYY-MM-DD');
             $scope.$apply();
