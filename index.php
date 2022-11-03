ce0555461b207a0b04e1454a78aaabc286c41942
Zia ur Rehman <zia.rehman@withloop.co>
2022-11-03 12:40:31 +0500

    updated code

--- index.php
+++ index.php
@@ -5,12 +5,13 @@ include_once dirname(__FILE__).'/include/settings.php';
 if(!empty($_SESSION['user_credentials']) && $_SESSION['user_credentials']['role'] == 'owner') {
     echo mainHeader(['page' => 'dashboard']);
     include_once dirname(__FILE__).'/pages/dashboard/owner.php';
+    echo mainFooter();
 }
 if(!empty($_SESSION['user_credentials']) && $_SESSION['user_credentials']['role'] == 'manager') {
     echo mainHeader(['page' => 'product']);
     include_once dirname(__FILE__).'/pages/dashboard/manager.php';
+    echo mainFooter();
 }
 if(!empty($_SESSION['user_credentials']) && $_SESSION['user_credentials']['role'] == 'shopkeeper') {
     include_once dirname(__FILE__).'/pages/recipt/index.php';
 }
-echo mainFooter();
