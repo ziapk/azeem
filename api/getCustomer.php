b3df8d5abbeeb8b11dde125feeecb048891d8665
Zia ur Rehman <zia.pccr@yahoo.com>
2023-02-24 16:00:10 +0500

    updated code

--- api/getCustomer.php
+++ api/getCustomer.php
@@ -1,9 +1,10 @@
 <?php 
 include_once dirname(__FILE__).'/../include/settings.php';
+global $shop;
 $customers = new  Customers();
 $search = [];
 if(!empty($_SESSION['shopInfo'])) {
-    $search = $customers->searchCustomer($_SESSION['shop']['id'], $_GET['term']);
+    $search = $customers->searchCustomer($shop['id'], $_GET['term']);
 };
 echo json_encode($search);
 ?>
