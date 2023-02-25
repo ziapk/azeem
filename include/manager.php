23f987964ec531894b8ffa5cf7611a0a98bffa23
Zia ur Rehman <zia.pccr@yahoo.com>
2023-02-25 13:18:12 +0500

    updated code

--- include/manager.php
+++ include/manager.php
@@ -20,7 +20,7 @@
         </div>
         <div class="pull-left welcome-header-section"><span>Welcome <strong><?php echo $userData['full_name'];?>!</strong></span></div>
       <ul class="list-inline navbar-right navbar-nav nav">
-        <div class="pull-left welcome-header-section sale-date" ng-if="getClass()"><button class="btn btn-danger" ng-click="applyClosing()"><span ng-class="{'blink': getClass()}">Sale Close</span></button></div>
+        <div class="pull-left welcome-header-section sale-date hide-temp" ng-show="getClass()"><button class="btn btn-danger" ng-click="applyClosing()"><span ng-class="{'blink': getClass()}">Sale Close</span></button></div>
         <li></li>
         <li><a href="<?php echo SITE_URL; ?>pages/product/running.php"><img width="22" uib-tooltip="Running Products" tooltip-placement="bottom" height="22" src="<?php echo SITE_URL; ?>assets/img/svg/lightning-bolt.svg" alt="" /></a></li>
         <li><a href="<?php echo SITE_URL; ?>pages/product/create.php"><img width="22" uib-tooltip="Add Product" tooltip-placement="bottom" height="22" src="<?php echo SITE_URL; ?>assets/img/svg/book.svg" alt="" /></a></li>
