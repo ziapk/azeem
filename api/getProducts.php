98017237676f74463d7452f62f32ade27541628a
Zia ur Rehman <zia.pccr@yahoo.com>
2023-03-02 20:19:01 +0500

    updated code

--- api/getProducts.php
+++ api/getProducts.php
@@ -14,11 +14,18 @@ $courceId = !empty($_GET['courceId']) ? $_GET['courceId'] : "";
 $publisher_id = !empty($_GET['publisher_id']) ? $_GET['publisher_id'] : "";
 $sortByField = !empty($_GET['sortByField']) ? $_GET['sortByField'] : "";
 $sortByOrder = !empty($_GET['sortByOrder']) ? $_GET['sortByOrder'] : "";
+$correction = !empty($_GET['correction']) ? $_GET['correction'] : false;
+$selectedPublisherId = !empty($_GET['selectedPublisherId']) ? $_GET['selectedPublisherId'] : false;
 $pin = !empty($_GET['bookmark']) ? $_GET['bookmark'] : "";
 
 $shopId = $userData['role'] == 'owner' ? null : $userData['shopId'];
 if(!empty($_SESSION['shopInfo'])) {
-    $search = $products->getOwnerProductsPagination($ownerId, ['page' => (int)$page, 'perPage' => (int)$perPage, 'search' => $search, 'searchBy' => $searchBy, 'courceId' => $courceId, 'group' => $group, 'board' => $board, 'full_name' => $full_name, 'author' => $author, 'sortByField' => $sortByField, 'sortByOrder' => $sortByOrder, 'pin' => $pin, 'publisher_id' => $publisher_id], $shopId);
+    if(!empty($selectedPublisherId) && !empty($correction)) {
+        $search = $products->assignProductsPublisher($ownerId, ['page' => (int)$page, 'perPage' => (int)$perPage, 'search' => $search, 'searchBy' => $searchBy, 'courceId' => $courceId, 'group' => $group, 'board' => $board, 'full_name' => $full_name, 'author' => $author, 'sortByField' => $sortByField, 'sortByOrder' => $sortByOrder, 'pin' => $pin, 'publisher_id' => $publisher_id, 'correction' => $correction, 'selectedPublisherId' => $selectedPublisherId], $shopId);
+    }
+    else {
+        $search = $products->getOwnerProductsPagination($ownerId, ['page' => (int)$page, 'perPage' => (int)$perPage, 'search' => $search, 'searchBy' => $searchBy, 'courceId' => $courceId, 'group' => $group, 'board' => $board, 'full_name' => $full_name, 'author' => $author, 'sortByField' => $sortByField, 'sortByOrder' => $sortByOrder, 'pin' => $pin, 'publisher_id' => $publisher_id, 'correction' => $correction], $shopId);
+    }
 };
 echo json_encode($search);
 ?>
