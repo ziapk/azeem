ec19b5d6c3a9830a6ff7b047a779639a7ee73972
Zia ur Rehman <zia.pccr@yahoo.com>
2023-03-16 00:19:37 +0500

    updated code

---
+++ api/getDemands.php
@@ -0,0 +1,22 @@
+<?php 
+//function __autoload($class_name) {
+spl_autoload_register(function ($class_name) {
+    //class directories
+    $directorys = array('classes/');
+    
+    //for each directory
+    foreach($directorys as $directory)
+    {
+        //see if the file exsists
+        if(file_exists(dirname(__FILE__).'/../'.$directory. strtolower($class_name). '.php'))
+        {
+            require_once(dirname(__FILE__).'/../'.$directory. strtolower($class_name).'.php');
+            //only require the class once, so quit after to save effort (if you got more, then name them something else 
+            return;
+        }            
+    }
+});
+$customers = new  Demands();
+$result = $customers->getStoreDemands($_GET['shop_id']);
+echo json_encode($result);
+?>
