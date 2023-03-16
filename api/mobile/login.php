79bdf35a149d1298994129b1dcdec551da2f879a
Zia ur Rehman <zia.pccr@yahoo.com>
2023-03-16 17:14:29 +0500

    updated code

--- api/mobile/login.php
+++ api/mobile/login.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__FILE__).'/autoload.php');
 if(empty($_POST['email']) || empty($_POST['password'])) {
-    http_response_code(400);
+    // http_response_code(400);
     echo json_encode(["message" => "Please fill all fields"]);
 }
 else {
@@ -13,14 +13,15 @@ else {
     $userData = $usersObj->login($email,$password);
     if(!empty($userData)) {
         if(is_array($userData)) {
+            // http_response_code(200);
             echo json_encode(["data" => $userData, "message" => "Successfully loggedin!"]);
         }
         else {
-            http_response_code(400);
+            // http_response_code(400);
             echo json_encode(["message" => $userData]);
         }
     } else {
-        http_response_code(400);
+        // http_response_code(400);
         echo json_encode(["message" => 'Invalid email or password.']);
     }
 }
