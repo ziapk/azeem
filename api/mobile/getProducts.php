9f3249e942ea0412f4e159597eefe5a95227d4c2
Zia ur Rehman <zia.pccr@yahoo.com>
2023-03-21 19:54:14 +0500

    updated mobile query

--- api/mobile/getProducts.php
+++ api/mobile/getProducts.php
@@ -3,7 +3,7 @@ require_once(dirname(__FILE__).'/autoload.php');
 $ownerId = $_GET['owner_id'];
 $products = new  Products();
 $page = !empty($_GET['page']) ? $_GET['page'] : 1;
-$perPage = !empty($_GET['perPage']) ? $_GET['perPage'] : 0;
+$perPage = !empty($_GET['perPage']) ? $_GET['perPage'] : 10;
 $search = !empty($_GET['search']) ? $_GET['search'] : "";
 $searchBy = !empty($_GET['searchBy']) ? $_GET['searchBy'] : "";
 $full_name = !empty($_GET['full_name']) ? $_GET['full_name'] : "";
@@ -24,6 +24,6 @@ if(!empty($selectedPublisherId) && !empty($correction)) {
     $search = $products->assignProductsPublisher($ownerId, ['page' => (int)$page, 'perPage' => (int)$perPage, 'search' => $search, 'searchBy' => $searchBy, 'courceId' => $courceId, 'group' => $group, 'board' => $board, 'full_name' => $full_name, 'author' => $author, 'sortByField' => $sortByField, 'sortByOrder' => $sortByOrder, 'pin' => $pin, 'dup' => $dup, 'publisher_id' => $publisher_id, 'correction' => $correction, 'selectedPublisherId' => $selectedPublisherId], $shopId);
 }
 else {
-    $search = $products->getOwnerProductsPagination($ownerId, ['page' => (int)$page, 'perPage' => (int)$perPage, 'search' => $search, 'searchBy' => $searchBy, 'courceId' => $courceId, 'group' => $group, 'board' => $board, 'full_name' => $full_name, 'author' => $author, 'sortByField' => $sortByField, 'sortByOrder' => $sortByOrder, 'pin' => $pin, 'dup' => $dup, 'publisher_id' => $publisher_id, 'correction' => $correction], $shopId, true);
+    $search = $products->getOwnerProductsPagination($ownerId, ['page' => (int)$page, 'perPage' => (int)$perPage, 'search' => $search, 'searchBy' => $searchBy, 'courceId' => $courceId, 'group' => $group, 'board' => $board, 'full_name' => $full_name, 'author' => $author, 'sortByField' => $sortByField, 'sortByOrder' => $sortByOrder, 'pin' => $pin, 'dup' => $dup, 'publisher_id' => $publisher_id, 'correction' => $correction], $shopId);
 }
 echo json_encode($search);
