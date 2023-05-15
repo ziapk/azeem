9d96e2c0d03e307312ae71b89a8ac0c1e3ba2d85
Zia ur Rehman <zia.pccr@yahoo.com>
2023-05-15 19:33:01 +0500

    return delete active

--- pages/orders/delete.php
+++ pages/orders/delete.php
@@ -3,15 +3,14 @@
 $id = !empty($_GET['id']) ? $_GET['id'] : null;
 $reason = !empty($_GET['reason']) ? $_GET['reason'] : null;
 
-if(empty($id) || empty($reason)) {
+if (empty($id) || empty($reason)) {
     echo json_encode(['success' => false, 'error' => 'Invalid order']);
 }
 
-include_once dirname(__FILE__).'/../../include/settings.php';
+include_once dirname(__FILE__) . '/../../include/settings.php';
 
-if(!empty($id) && !empty($reason)) {
+if (!empty($id) && !empty($reason)) {
     $orders = new Orders();
-    $orders->changeOrderFlag(['id' => $id, 'reason' => $reason, 'flag' => 2]);
+    $orders->changeOrderFlag(['id' => $id, 'reason' => $reason, 'flag' => 99]);
     echo json_encode(['success' => true, 'message' => "Successfully deleted!"]);
 }
-?>
