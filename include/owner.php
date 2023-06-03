80b4279d9a942f5c06902276f9ae1f85cd7c9769
Zia ur Rehman <zia.pccr@yahoo.com>
2023-06-03 11:02:29 +0500

    added stationary in menu

--- include/owner.php
+++ include/owner.php
@@ -43,6 +43,29 @@ $customersList = $customerObj->getCustomers($shop['id']);
 
         <ul class="list-inline navbar-right navbar-nav nav">
           <li class="dropdown" style="padding: 0">
+            <a href="#" class="nav-menu-item btn btn-primary" data-toggle="dropdown">
+              <img src="<?php echo SITE_URL . 'assets/img/stationary.png'; ?>" alt="" width="40" height="40" style="margin: -10px 0" />
+            </a>
+            <ul class="dropdown-menu">
+              <?php foreach ($productCategories as $key => $value) { ?>
+                <li class="dropdown">
+                  <a style="padding: 3px 6px" class="dropdown-item" href="#" data-toggle="dropdown"><span class="nav-menu-icon" style="margin-right: 6px"><img width="30" height="30" src="<?php echo SITE_URL . 'uploads/products/' . $value['image']; ?>" alt="" /></span><span class="nav-menu-text"><?php echo $value['full_name']; ?></span>
+                    <div class="fa fa-caret-right"></div>
+                  </a>
+                  <?php if (!empty($categoryProducts[$value['id']])) { ?>
+                    <ul class="dropdown-menu dropdown-submenu" style="min-width: 250px; max-height: 300px; overflow: auto">
+                      <?php foreach ($categoryProducts[$value['id']] as $c) { ?>
+                        <li>
+                          <a ng-click='addToCart(<?php echo safe_json_encode($c); ?>)' style="padding: 3px 6px" class="dropdown-item" href="#"><code class="nav-menu-text"><?php echo $c['price']; ?></code><span class="nav-menu-text" style="white-space: normal"><?php echo $c['full_name']; ?></span></a>
+                        </li>
+                      <?php } ?>
+                    </ul>
+                  <?php } ?>
+                </li>
+              <?php } ?>
+            </ul>
+          </li>
+          <li class="dropdown" style="padding: 0">
             <a href="#" class="nav-menu-item btn btn-primary" data-toggle="dropdown" uib-tooltip="Receivings" tooltip-placement="bottom" title="">
               Re.
             </a>
