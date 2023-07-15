9b84354b3a3490c3c5ad81296d7aa7479625fbae
Zia ur Rehman <zia.pccr@yahoo.com>
2023-07-15 13:56:29 +0500

    updated code

--- pages/recipt/index.php
+++ pages/recipt/index.php
@@ -65,12 +65,6 @@ $ownerStores = $stores->getOwnerStores($userId);
                                 <th width="100">
                                     <div class="dropdown-wrapper align-right">
                                         <input type="text" class="form-control" id="searchProduct" ng-model="product" placeholder="Search Products" uib-typeahead="address as address.full_name for address in searchProduct($viewValue)" typeahead-on-select="selectProduct($item)" ng-model-options="{debounce: 100}" typeahead-template-url="row.html" class="form-control" typeahead-show-hint="true" typeahead-min-length="1">
-                                        <!-- <div class="list-group recipt-search-dropdown">
-                        <a ng-click="selectProduct(l)" class="list-group-item" ng-repeat="l in list">
-                            <h4 class="list-group-item-heading">{{l.full_name}} <span>{{l.price}}</span></h4>
-                        </a>
-                        <a ng-if="list.length" ng-click="clearSearch()" class="list-group-item">Close</a>
-                    </div> -->
                                     </div>
                                 </th>
                             </tr>
