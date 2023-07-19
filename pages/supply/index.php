9251ed7eabe865aa69c77caaf260d658058890c9
Zia ur Rehman <zia.pccr@yahoo.com>
2023-07-19 19:24:03 +0500

    updated code

--- pages/supply/index.php
+++ pages/supply/index.php
@@ -204,6 +204,7 @@ echo mainFooter();
 
             $timeout(() => {
                 if ($scope.qf) {
+                    $anchorScroll.yOffset = 100;
                     $location.hash('product-' + currentIndex);
                     $anchorScroll();
                     $('#product-' + currentIndex).find('.discount-field').focus();
