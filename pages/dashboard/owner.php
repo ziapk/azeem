4971323250c1a5cffa2cc2852c194ac4ab9133c8
Zia ur Rehman <zia.pccr@yahoo.com>
2023-07-19 21:10:45 +0500

    updated code

--- pages/dashboard/owner.php
+++ pages/dashboard/owner.php
@@ -327,10 +327,10 @@ foreach ($publishersArr as $key => $value) {
             <div uib-alert ng-if="alert" ng-class="'alert-'+(alert.type || 'warning')" close="closeAlert()">{{alert.message}}</div>
             <div class="form-group">
                 <label for="sname">Opening Balance</label>
-                <input id="sname" type="text" ng-model="form.closing_balance" class="form-control" placeholder="Closing Balance">
+                <input id="sname" type="text" ng-model="form.closing_balance" class="form-control" placeholder="Opening Balance">
             </div>
             <div class="form-group">
-                <label>Contact</label>
+                <label>Set Next Opening Date</label>
                 <input date-range-picker class="form-control date-picker" type="text" ng-model="form.sale_date" options="{ autoApply: true, singleDatePicker: true }">
             </div>
         </div>
