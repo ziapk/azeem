748a94dc257b8cfbd6c1ee6437c2e3a41d7378d5
Zia ur Rehman <zia.pccr@yahoo.com>
2023-07-24 15:38:38 +0500

    added quick actions for courses

--- pages/recipt/index.php
+++ pages/recipt/index.php
@@ -24,12 +24,13 @@ $ownerStores = $stores->getOwnerStores($userId);
             <a style="padding: 3px 6px" class="dropdown-item btn btn-primary" href="#" data-toggle="dropdown"><span class="nav-menu-icon" style="margin-right: 6px"></span><span class="nav-menu-text">Syllabus</span>
                 <div class="fa fa-caret-right"></div>
             </a>
-            <ul style="min-width: 250px; max-height: 300px;">
+            <ul class="dropdown-menu" style="min-width: 250px; max-height: 300px;">
                 <li ng-if="pinPrograms" ng-repeat="(key, li) in pinPrograms">
                     <a style="padding: 3px 6px" class="dropdown-item" href="#"><code class="nav-menu-text"></code><span class="nav-menu-text" style="white-space: normal">{{key}}</span></a>
                     <ul class="dropdown-menu dropdown-submenu" style="min-width: 250px; max-height: 300px;">
                         <li ng-repeat="(k, l) in li">
                             <a href="javascript:void(0)" ng-class="{'text-danger text-bold': selectedSize == k }">{{k}}</a>
+
                             <ul class="dropdown-menu dropdown-submenu" style="min-width: 250px; max-height: 300px;">
                                 <li ng-repeat="(dd, i) in l">
                                     <a href="javascript:void(0)" ng-click="addAllBooks(i.items)">{{dd}}</a>
