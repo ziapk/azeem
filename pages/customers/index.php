f86c563b64bb7eb4ac2eb4aa47137a7ef355ad4a
Zia ur Rehman <zia.pccr@yahoo.com>
2023-08-05 14:59:19 +0500

    updated code

--- pages/customers/index.php
+++ pages/customers/index.php
@@ -27,7 +27,7 @@ echo mainHeader(['page' => 'customer']);
                 <td><strong>{{li.full_name}}</strong> <br /> {{li.phoneNumber}}</td>
                 <td><strong>{{li.company}}</strong> - {{li.title}} <br />{{li.address}}</td>
                 <td>{{li.type == '2' ? 'NO': 'YES'}}</td>
-                <td ng-class="{'text-danger': li.closing_balance < 0}">{{li.closing_balance}}</td>
+                <td style="text-align: right;" ng-class="{'text-danger': li.closing_balance < 0}">{{li.closing_balance | number}}</td>
                 <td>
                     <?php if ($userData['role'] === 'owner' || $userData['role'] === 'manager') { ?><a class="btn btn-info btn-xs" href="javascript:void(0)" ng-click="assignBooks(li)">Disc.</a><?php } ?>
                     <?php if ($userData['role'] === 'owner' || $userData['role'] === 'manager') { ?><a class="btn btn-default btn-xs" href="../chart-of-accounts/summery.php?t=c&id={{li.account_id}}">Ledger</a><?php } ?>
