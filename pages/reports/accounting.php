ebb70a03f8dc3bd55d807f38beb78ace3f5b7459
Zia ur Rehman <zia.pccr@yahoo.com>
2023-08-09 14:25:15 +0500

    updated code

--- pages/reports/accounting.php
+++ pages/reports/accounting.php
@@ -1,6 +1,9 @@
 <?php
 
 include_once dirname(__FILE__) . '/../../include/settings.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $doubleEntry = new DoubleEntry();
 
