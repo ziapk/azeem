01b816ea517fca528814f4f8893a8afd96d41cff
Zia ur Rehman <zia.pccr@yahoo.com>
2023-08-15 13:58:06 +0500

    updated code

--- pages/reports/salesProductsReport.php
+++ pages/reports/salesProductsReport.php
@@ -29,7 +29,7 @@ $totals = ['price' => 0, 'discount' => 0, 'paid' => 0, 'balance' => 0];
         ?>
             <tr>
                 <td><?php echo $count; ?></td>
-                <td><?php echo dateToSimple($s['order_date']); ?></td>
+                <td><?php echo dateToSimple(date('Y-m-d', strtotime($s['order_date']))); ?></td>
                 <td><?php echo $s['order_custom_id']; ?></td>
                 <td><?php echo !empty($s['full_name']) ? $s['full_name'] : $s['name']; ?></td>
                 <td><?php echo $s['productName']; ?></td>
