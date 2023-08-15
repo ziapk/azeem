2564bf9627d40bdb755210e1eb5e71c40eaf0d0f
Zia ur Rehman <zia.pccr@yahoo.com>
2023-08-15 18:53:50 +0500

    updated code

--- classes/connection.php
+++ classes/connection.php
@@ -8,6 +8,12 @@ class Connection
 	private $user = 'reclydmy_pos';
 	private $pass = ';4B)pQC=K0&v';
 
+
+	// private $host = 'localhost';
+	// private $dbname = 'reclydmy_azeem';
+	// private $user = 'root';
+	// private $pass = 'root';
+
 	public $dbh;
 
 	function __construct()
@@ -31,6 +37,18 @@ class Connection
 		$_GET['largeView'] = 'large';
 		include dirname(__FILE__) . '/../print/index.php';
 		$html = ob_get_clean();
+		ob_clean();
+		return $html;
+	}
+	public function drawSupply($id)
+	{
+		ob_start();
+		$_GET['id'] = $id;
+		$_GET['detail'] = 'true';
+		$_GET['largeView'] = 'large';
+		include dirname(__FILE__) . '/../print/supply.php';
+		$html = ob_get_clean();
+		ob_clean();
 		return $html;
 	}
 	public function normalToPassword($password)
