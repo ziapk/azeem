1e46d014cc865b3bf28b36f89f56c99fa84e2bb6
Zia ur Rehman <zia.pccr@yahoo.com>
2023-08-29 15:47:19 +0500

    updated sum

--- pages/orders/index.php
+++ pages/orders/index.php
@@ -72,7 +72,7 @@ echo mainHeader(['page' => 'order']);
                 <td>{{row.order_custom_id}}</td>
                 <td>{{row.customer_name || row.full_name}}</td>
                 <td>{{row.price - row.discount | number: 0}}</td>
-                <td ng-repeat="mode in modes">{{row.prices[mode.id]}}</td>
+                <td ng-repeat="mode in modes">{{row.prices[mode.id] | number: 0}}</td>
                 <td>{{row.runningTotal | number: 0}}</td>
                 <td>{{statusArr[row.status].full_name}}</td>
                 <td>{{row.order_date}}</td>
