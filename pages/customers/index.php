01f189806ff26b8fcd5dea5085f27acbc0d235f5
Zia ur Rehman <zia.pccr@yahoo.com>
2023-10-01 14:08:57 +0500

    updated code

--- pages/customers/index.php
+++ pages/customers/index.php
@@ -31,6 +31,7 @@ echo mainHeader(['page' => 'customer']);
                 <td>
                     <?php if ($userData['role'] === 'owner' || $userData['role'] === 'manager') { ?><a class="btn btn-info btn-xs" href="javascript:void(0)" ng-click="assignBooks(li)">Disc.</a><?php } ?>
                     <?php if ($userData['role'] === 'owner' || $userData['role'] === 'manager') { ?><a class="btn btn-default btn-xs" href="../chart-of-accounts/summery.php?t=c&id={{li.account_id}}">Ledger</a><?php } ?>
+                    <?php if ($userData['role'] === 'owner' || $userData['role'] === 'manager') { ?><a class="btn btn-default btn-xs" ng-click="sendSummery(li.account_id)" href="javascript:void(0)"><span class="fa fa-envelope"></span>{{sending[li.account_id] ? 'Sending' : ''}}</a><?php } ?>
                     <?php if ($userData['role'] === 'owner' || $userData['role'] === 'manager') { ?><a class="btn btn-xs btn-primary" href="adjustment.php?id={{li.account_id}}">Receiving</a><?php } ?>
                     <?php if ($userData['role'] === 'manager') { ?><a class="btn btn-danger btn-xs" href="<?php echo SITE_URL; ?>pages/orders/customerOrders.php?id={{li.id}}">Orders</a><?php } ?>
                     <?php if ($userData['role'] === 'owner' || $userData['role'] === 'manager') { ?><a class="btn btn-default btn-xs" href="<?php echo SITE_URL; ?>pages/customers/update.php?id={{li.id}}"><span class="fa fa-edit"><span></a><?php } ?>
@@ -67,6 +68,44 @@ echo mainFooter();
         $scope.list = []; //$scope.data.records;
         $scope.search = ""; //$scope.data.records;
         $scope.siteUrl = '<?php echo SITE_URL ?>';
+        $scope.sending = {};
+
+        $scope.sendSummery = (account_id) => {
+            $scope.sending[account_id] = true;
+            $http.post($scope.siteUrl + 'pages/chart-of-accounts/sendSummery.php', $httpParamSerializerJQLike({
+                account_id,
+                from: '',
+                to: '',
+                type: 'c',
+                customer_name: "<?php echo $user['full_name']; ?>",
+            }), {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function(res) {
+                $scope.sending[account_id] = false;
+                if (res.data.success) {
+                    $scope.alert = {
+                        type: 'success',
+                        message: res.data.message
+                    }
+                } else {
+                    $scope.alert = {
+                        type: 'danger',
+                        message: res.data.message
+                    }
+                }
+                // $uibModalInstance.close($scope.form);
+            }).catch(err => {
+
+                $scope.alert = {
+                    type: 'danger',
+                    message: err?.message || err
+                }
+
+                $scope.sending[account_id] = false;
+            });
+        }
 
         $scope.getCustomers = (page) => {
             $scope.loading = true;
