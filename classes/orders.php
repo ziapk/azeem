de873ead794517f0f562193967fde2370c1eba60
Zia ur Rehman <zia.pccr@yahoo.com>
2024-02-27 00:53:40 +0500

    updated report

--- classes/orders.php
+++ classes/orders.php
@@ -1310,7 +1310,7 @@ class Orders extends Connection
                 $toCondition .= " and p.id IN( $ids ) ";
             }
 
-            $stmt = "SELECT rp.product_id, o.return_type, rp.price AS price, sum(rp.quantity) AS quantity, p.full_name  FROM `{$this->table_ro}` AS o LEFT JOIN `{$this->table_rp}` AS rp ON rp.order_id = o.id LEFT JOIN `{$this->table_sub}` AS oi ON o.id=oi.order_id and oi.id=rp.product_id LEFT JOIN `{$this->table_pro}` AS p on p.id=oi.product_id  WHERE o.shopId=:shopId " . $toCondition . ' and o.flag IN (1, 2) group by o.return_type, rp.product_id';
+            $stmt = "SELECT rp.product_id, p.publisher_id, o.return_type, rp.price AS price, sum(rp.quantity) AS quantity, p.full_name  FROM `{$this->table_ro}` AS o LEFT JOIN `{$this->table_rp}` AS rp ON rp.order_id = o.id LEFT JOIN `{$this->table_pro}` AS p on p.id=rp.product_id  WHERE o.shopId=:shopId " . $toCondition . ' and o.flag IN (1, 2) group by o.return_type, rp.product_id';
             $prepare = $this->dbh->prepare($stmt);
             $prepare->bindParam(':shopId', $shopId, PDO::PARAM_STR);
             $prepare->execute();
