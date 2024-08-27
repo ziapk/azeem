422a05d8b027297ffd69655bd17efc8ebcc5165c
Zia ur Rehman <zia.pccr@yahoo.com>
2024-08-28 00:18:33 +0500

    updated code

--- api/updateProfile.php
+++ api/updateProfile.php
@@ -15,7 +15,10 @@ $user = $users->updateProfile($_POST);
 
 if ($user) {
     echo json_encode(['success' => true, 'message' => 'Profile Updated']);
+    if (!empty($_SESSION['user_credentials']) && $_SESSION['user_credentials']['role'] == 'superadmin') {
+    } else {
     $_SESSION['user_credentials'] = $_POST;
+    }
 } else {
     echo json_encode(['success' => false, 'message' => 'Nothing Change']);
 }
