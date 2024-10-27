5b067026660032d4c56d75e6f3ae04488707d13e
Mac <zia@dealer230i.com>
2024-10-27 21:29:38 +0500

    debug sort

--- classes/products.php
+++ classes/products.php
@@ -263,7 +263,7 @@ class Products extends Connection
 					$sortByQry = " ORDER BY p.price " . $order;
 				}
 				if ($name == 'stock') {
-					$sortByQry = " ORDER BY p.in_hand " . $order;
+					$sortByQry = " ORDER BY qty " . $order;
 				}
 			}
 
@@ -303,10 +303,10 @@ class Products extends Connection
 			$offset =  ((!empty($currentPage) ? $currentPage : 1) - 1) * $no_of_records_per_page;
 
 			$stmt = "SELECT *, price, concat(`id`, '|', `price`, '|', `full_name`, '|', COALESCE(`publisherName`, ''), '|', COALESCE(`author`, ''), '|', COALESCE(`board`, ''), '|', COALESCE(`code`, ''), '|', COALESCE(`barcode`, ''), '|', COALESCE(`other_codes`, '')) as searchString FROM (SELECT $mainCols  FROM `{$this->table_st}` as sp $innerJoin LEFT JOIN {$this->pc_table} as pc ON pc.product_id = p.id LEFT JOIN {$this->table_rack_products} as rp ON rp.product_id = p.id LEFT JOIN `{$this->table_rack}` as r on r.id = rp.rack_id LEFT JOIN program_books as c ON c.product_id = p.id LEFT JOIN publishers as pub on p.publisher_id = pub.id  WHERE p.`owner_id`=:owner_id $status_query $publisher_query $dup $type $pin $searchQry $catQry GROUP BY p.id $sortByQry $minQry LIMIT :offset, :perPage) AS b";
-			if ($name == 'stock') {
-				echo $stmt;
-				exit;
-			}
+			// if ($name == 'stock') {
+			// 	echo $stmt;
+			// 	exit;
+			// }
 			$prepare = $dbh->prepare($stmt);
 			$prepare->bindParam(':owner_id', $owner_id, PDO::PARAM_STR);
 			$prepare->bindParam(':offset', $offset, PDO::PARAM_INT);
