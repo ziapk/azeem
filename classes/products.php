4e939a2e5c90d219f204d2fcc7150dc9782544ab
Zia ur Rehman <zia.pccr@yahoo.com>
2025-02-15 17:30:11 +0500

    updated query

--- classes/products.php
+++ classes/products.php
@@ -1208,9 +1208,6 @@ class Products extends Connection
 	{
 		$dbh = $this->connectionPool->getConnection();
 		try {
-
-			print_r($array);
-			
 			$stmt = "UPDATE `{$this->table_st}` SET `qty`=(COALESCE(:qty, 0) + COALESCE(stock_out, 0)) WHERE product_id=:product_id and shopId = :shopId";
 
 			$prepare = $dbh->prepare($stmt);
