b598ae02cef86397d7b8191322c73bde0c7ba359
Zia ur Rehman <zia.pccr@yahoo.com>
2025-09-03 16:06:33 +0500

    debug-return-delete-2

--- pages/orders/returnDelete.php
+++ pages/orders/returnDelete.php
@@ -1,16 +1,16 @@
 <?php
 
-$id = !empty($_GET['id']) ? $_GET['id'] : null;
+
+include_once dirname(__FILE__) . '/../../include/settings.php';
+$currentReturnDeleteID = !empty($_GET['id']) ? $_GET['id'] : null;
 $reason = !empty($_GET['reason']) ? $_GET['reason'] : null;
 
 if (empty($id) || empty($reason)) {
     echo json_encode(['success' => false, 'error' => 'Invalid order']);
 }
 
-include_once dirname(__FILE__) . '/../../include/settings.php';
-
-if (!empty($id) && !empty($reason)) {
+if (!empty($currentReturnDeleteID) && !empty($reason)) {
     $orders = new Orders();
-    $orders->changeReturnFlag(['id' => $id, 'reason' => $reason, 'flag' => 99]);
+    $orders->changeReturnFlag(['id' => $currentReturnDeleteID, 'reason' => $reason, 'flag' => 99]);
     echo json_encode(['success' => true, 'message' => "Successfully deleted!"]);
 }
